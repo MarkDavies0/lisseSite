b3f140a3ab8ce4c8586e081d4466c335f185c178
Mark <71405899+MarkDavies0@users.noreply.github.com>
2020-09-29 19:48:04 -0700

    files for site

---
+++ script.js
@@ -0,0 +1,19 @@
+function hoverImg(element) {
+    element.setAttribute('src', 'firstShotLIGHT.jpg');
+  }
+  
+  function unhoverImg(element) {
+    element.setAttribute('src', 'firstShotDARK.jpg');
+  }
+
+  function hoverSvg(element) {
+    let ele = element.getElementsByTagName("IMG")[0];
+    ele.setAttribute('src', 'icons/flagBlue.png');
+    ele.style.transform = 'scale(1.1)';
+  }
+  
+  function unhoverSvg(element) {
+    let ele = element.getElementsByTagName("IMG")[0];
+    ele.setAttribute('src', 'icons/flagRegular.png');
+    ele.style.transform = 'scale(1.0)';
+  }
